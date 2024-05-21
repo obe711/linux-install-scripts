067c214c7ab8a337acf200504f8602464dfdd3ff
Obediah Klopfenstein <obe711@gmail.com>
2024-05-20 20:11:51 -0500

    install all

---
+++ install_all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -o- https://raw.githubusercontent.com/obe711/linux-install-scripts/main/install_misc.sh | bash
+curl -o- https://raw.githubusercontent.com/obe711/linux-install-scripts/main/install_docker.sh | bash
+curl -o- https://raw.githubusercontent.com/obe711/linux-install-scripts/main/install_mongo.sh | bash
+curl -o- https://raw.githubusercontent.com/obe711/linux-install-scripts/main/install_build-tools.sh | bash
