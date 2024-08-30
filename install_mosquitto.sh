d732ad0839a356bc8296ebbc0f07731b34d3681e
Benjamin Klopfenstien <obe711@gmail.com>
2024-08-30 16:37:04 -0500

    Update install_mosquitto.sh

--- install_mosquitto.sh
+++ install_mosquitto.sh
@@ -2,6 +2,8 @@
 
 sudo apt update -y && sudo apt install mosquitto mosquitto-clients -y
 
+sudo apt-get install libmosquitto-dev
+
 sudo systemctl status mosquitto
 
 mkdir /home/nexus/mosquitto-config
